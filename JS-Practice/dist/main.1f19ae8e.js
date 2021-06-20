eed026669a3afa2d10a1937c2061af3601476292
toproot <topgeun7913@naver.com>
2021-06-21 00:12:30 +0900

    JS 연산

--- JS-Practice/dist/main.1f19ae8e.js
+++ JS-Practice/dist/main.1f19ae8e.js
@@ -118,9 +118,20 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"main.js":[function(require,module,exports) {
-// 반복문 (For statement)
-// for (시작조건; 종료조건; 변화조건) {}
-for (var i = 0; i < 3; i += 1) {}
+// 형 변환(Type conversion)
+var a = 1;
+var b = '1'; // == : 동등 연산자
+// 되도록이면 안쓰는 것 권장. 예외사황 발생(자동 형변환 발생)
+// 웬만하면 일치연산자(===) 사용하기
+
+console.log(a == b); // Truthy(참 같은 값)
+// true, {}, [], 1, 2, 'false', -12, '3.14' ...
+// Falsy(거짓 같은 값)
+// false, '', null, undefiend, 0, -0, NaN(Not a Number => 1 + undefined)
+
+if ('false') {
+  console.log(123);
+}
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- JS-Practice/main.js
+++ JS-Practice/main.js
@@ -1,6 +1,19 @@
-// 반복문 (For statement)
-// for (시작조건; 종료조건; 변화조건) {}
+// 형 변환(Type conversion)
 
-for (let i = 0; i < 3; i += 1) {
-  
+const a = 1
+const b = '1'
+
+// == : 동등 연산자
+// 되도록이면 안쓰는 것 권장. 예외사황 발생(자동 형변환 발생)
+// 웬만하면 일치연산자(===) 사용하기
+console.log(a == b)
+
+// Truthy(참 같은 값)
+// true, {}, [], 1, 2, 'false', -12, '3.14' ...
+
+// Falsy(거짓 같은 값)
+// false, '', null, undefiend, 0, -0, NaN(Not a Number => 1 + undefined)
+
+if ('false') {
+  console.log(123)
 }
