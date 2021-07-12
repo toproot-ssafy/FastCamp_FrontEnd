54755fd7d27dbed1730789cf80fd67253b6b13a5
toproot <topgeun7913@naver.com>
2021-07-12 22:32:57 +0900

    JS 연산자 연습

--- JS-Practice/test.js
+++ JS-Practice/test.js
@@ -1,16 +1,21 @@
-const user = {
-  name: 'HEROPY',
-  age: 85,
-  eamils: [
-    'thesecon@gmail.com',
-    'neo@zillinks.com'
-  ]
-}
-console.log('user', user)
+console.log(typeof 'Hello World!') // string
+console.log(typeof 123) // number
+console.log(typeof true) // boolean
+console.log(typeof undefined) // undefined
 
-const str = JSON.stringify(user)
-console.log('str : ', str)
-console.log(typeof str)
+// null {} [] => object
+console.log(typeof null) // object
+console.log(typeof {}) // object
+console.log(typeof []) // object
 
-const obj = JSON.parse(str)
-console.log('obj : ', obj)
+// typeof 함수 만들기
+// slice메소드로 특정 데이터의 상세 타입만을 출력
+function getType(data) { 
+   return Object.prototype.toString.call(data).slice(8, -1)
+ }
+
+console.log(getType(123)) // Number
+console.log(getType(false)) // Boolean
+console.log(getType(null)) // Null
+console.log(getType({})) // Object
+console.log(getType([])) // Array
